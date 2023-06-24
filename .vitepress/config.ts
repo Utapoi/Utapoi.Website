ba0819203439a9f2d27c29d10916528a5adbce8f
Florian Theron <mikyan0207@gmail.com>
2023-06-24 20:12:27 +0200

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: 'utapoi.github.io',
+  base: 'utapoi.com',
   title: "Utapoi",
   description: "Karaoke straight to your screen.",
   themeConfig: {
